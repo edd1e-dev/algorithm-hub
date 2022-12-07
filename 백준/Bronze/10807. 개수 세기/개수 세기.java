75021c047eabfbc1a95d106016c257ffca0b27c0
Eddie <56020202+edd1e-dev@users.noreply.github.com>
2022-12-07 21:21:21 +0900

    [Bronze V] Title: 개수 세기, Time: 132 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,28 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int v = Integer.parseInt(br.readLine());
+
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == v) result++;
+        }
+
+        System.out.println(result);
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
