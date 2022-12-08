211476862fd1da9adcd817c4cdf00e62851f481d
Eddie <56020202+edd1e-dev@users.noreply.github.com>
2022-12-08 18:44:30 +0900

    [Gold IV] Title: 문자열 폭발, Time: 600 ms, Memory: 62712 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,51 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        String bomb = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+
+            if (stack.size() >= bomb.length()) {
+                boolean match = true;
+                for (int j = 0; j < bomb.length(); j++) {
+                    if (stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match) {
+                    for (int j = 0; j < bomb.length(); j++) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        if (!stack.isEmpty()) {
+            for (Character c : stack) {
+                sb.append(c);
+            }
+            bw.write(sb.toString());
+        } else {
+            bw.write("FRULA");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
